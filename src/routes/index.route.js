57ee70d57d0689c1f77b615941fecfb7eb6bc954
NishaMajhi <nishamajhi0@gmail.com>
2024-02-01 18:19:00 +0530

    make notes

--- src/routes/index.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import userRouter from "./user.route.js";
-
-const indexRouter = Router();
-
-//user routes
-indexRouter.use('/users', userRouter);
-
-export default indexRouter
